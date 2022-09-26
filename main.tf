3796a8cdb32df408b0e50689340eb059f9d77603
MechwidIt <111199754+MechwidIt@users.noreply.github.com>
2022-09-26 23:16:59 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+variable "iam_user_name_prefix" {
+	default = "my_iam_user"
+}
+
+provider "aws"  {
+  region = "us-east-1"
+}
+
+
+resource "aws_iam_user" "my_iam_users" {
+	count = 2
+  	name = "${var.iam_user_name_prefix}_${count.index}"
+}
+
